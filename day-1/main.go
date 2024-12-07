d8824d7a855c6caf98794cc1f76137d596979f66
korneldabrowski <korneldabrowski@gmail.com>
2024-12-07 15:39:58 +0100

    implementation

--- day-1/main.go
+++ day-1/main.go
@@ -1,11 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"aoc-2024-day-1/utils"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, Worlds!")
+	fmt.Printf("The distance is: %v", findOutDistanceToSanta("puzzle_input.txt"))
 }
 
-func findOutDistanceToSanta(fileLocation string) (int, error) {
-	return 0, nil
+func findOutDistanceToSanta(fileLocation string) int {
+
+	leftList, rightList := readInputFile(fileLocation)
+
+	if len(leftList) != len(rightList) {
+		fmt.Printf("Something went horribly wrong...The left & right sides aren't equal!")
+	}
+
+	sortedLeft := utils.QuickSortStart(leftList)
+	sortedRight := utils.QuickSortStart(rightList)
+
+	totalDistance := 0
+
+	for i := 0; i < len(sortedLeft); i++ {
+		totalDistance += fintOutDifference(sortedLeft[i], sortedRight[i])
+	}
+
+	return totalDistance
+}
+
+func fintOutDifference(left, right int) int {
+	fmt.Printf("Left: %v, Right: %v\n", left, right)
+	if left > right {
+		fmt.Printf("Left is greater. Difference: %v\n", left-right)
+		return left - right
+	} else {
+		fmt.Printf("Right is greater. Difference: %v\n", right-left)
+		return right - left
+	}
+}
+
+func readInputFile(fileLocation string) ([]int, []int) {
+	file, err := os.Open(fileLocation)
+
+	if err != nil {
+
+		fmt.Errorf("Error: %v", err)
+		return nil, nil
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	leftList := make([]int, 0)
+	rightList := make([]int, 0)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		line = strings.TrimSuffix(line, "\n")
+
+		splitted := strings.Split(line, "   ")
+
+		i, err := strconv.ParseInt(splitted[0], 10, 64)
+		if err != nil {
+			fmt.Errorf("Error: %v", err)
+		}
+		leftList = append(leftList, int(i))
+
+		i, err = strconv.ParseInt(splitted[1], 10, 64)
+		if err != nil {
+			fmt.Errorf("Error: %v", err)
+		}
+		rightList = append(rightList, int(i))
+
+	}
+
+	return leftList, rightList
 }
--- day-1/utils/quicksort_test.go
+++ day-1/utils/quicksort_test.go
@@ -7,9 +7,9 @@ import (
 
 func TestQuickSort(t *testing.T) {
 
-	expectedArray := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expectedArray := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	actualArray := QuickSortStart([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
+	actualArray := QuickSortStart([]int64{10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
 
 	if !reflect.DeepEqual(expectedArray, actualArray) {
 		t.Errorf("Expected array to be %v, but got %v", expectedArray, actualArray)
